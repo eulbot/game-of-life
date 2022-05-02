5f41815ce0e911ba706b13d097c929609c2bb86f
Ulrich Fischer <careforthegoodones@gmail.com>
2022-05-02 19:38:55 +0200

    Remove gol code from main

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,29 +1,16 @@
-function getNeighbours(gol: boolean[][], col: number, row: number): number[][] {
+function getNeighbours(gol: boolean[][], col: number, row: number): Array<{c: number, r: number}> {
   const rows = gol.length - 1;
   const cols = gol[0].length - 1;
 
   return [col == 0 ? rows : col - 1, col, col >= rows ? 0 : col + 1]
-    .flatMap((c) => [row == 0 ? cols : row - 1, row, row >= cols ? 0 : row + 1].map((r) => [c, r]))
-    .filter((v) => !(v[0] == col && v[1] == row));
+    .flatMap((c) => [row == 0 ? cols : row - 1, row, row >= cols ? 0 : row + 1].map((r) => ({ c: c, r: r })))
+    .filter((v) => !(v.c == col && v.r == row));
 }
 
-const neighbors = (gol: boolean[][], col: number, row: number) => {
-  const width = gol.length;
-  const height = gol[0].length;
-  let result = 0;
-
-  const cols = [col == 0 ? width - 1 : col - 1, col, col + 1 >= width ? 0 : col + 1];
-  const rows = [row == 0 ? height - 1 : row - 1, row, row + 1 >= height ? 0 : row + 1];
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (!(i == 1 && j == 1)) {
-        result += gol[cols[i]][rows[j]] ? 1 : 0;
-      }
-    }
-  }
-  return result;
-};
+function getLiveCount(gol: boolean[][], col: number, row: number) {
+  const neighbors = getNeighbours(gol, col, row);
+  return neighbors.reduce((p, n) => p + (gol[n.c][n.r] ? 1 : 0), 0);
+}
 
 function setupBoard(cols: number, rows: number, seed?: number) {
   return Array<boolean>(cols)
@@ -38,7 +25,14 @@ function setupBoard(cols: number, rows: number, seed?: number) {
 function evolve(gol: boolean[][]): boolean[][] {
   let prev = gol.map((inner) => inner.slice());
 
-  return prev;
+  for (let x = 0; x < gol.length; x++) {
+    for (let y = 0; y < gol[x].length; y++) {
+      const n = getLiveCount(prev, x, y);
+      if (gol[x][y] == false && n == 3) gol[x][y] = true;
+      if (gol[x][y] == true && !(n == 2 || n == 3)) gol[x][y] = false;
+    }
+  }
+  return gol;
 }
 
-export { getNeighbours, setupBoard };
+export { getNeighbours, setupBoard, evolve };
--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,35 @@
+import { evolve, setupBoard } from './game-of-life';
+
 const SQAURE_SIZE = 20;
+const canvas = document.getElementById('playground') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
 
-function setupGame(width: number, height: number) {
-  const x = Math.ceil(width / SQAURE_SIZE);
-  const y = Math.ceil(height / SQAURE_SIZE);
-  return Array<number>(x)
-    .fill(0)
-    .map(() =>
-      Array<number>(y)
-        .fill(0)
-        .map(() => (Math.random() > 0.85 ? 1 : 0))
-    );
-}
-
-function drawBoard(gol: number[][]) {
-  const canvas = document.getElementById("playground") as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d");
-
-  ctx.fillStyle = "black";
+function drawBoard(gol: boolean[][]) {
+  ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let x = 0; x < gol.length; x++) {
     for (let y = 0; y < gol[x].length; y++) {
-      ctx.fillStyle = gol[x][y] ? "black" : "white";
-      ctx.fillRect(
-        x * SQAURE_SIZE,
-        y * SQAURE_SIZE,
-        SQAURE_SIZE - 1,
-        SQAURE_SIZE - 1
-      );
+      ctx.fillStyle = gol[x][y] ? 'black' : 'white';
+      ctx.fillRect(x * SQAURE_SIZE, y * SQAURE_SIZE, SQAURE_SIZE - 1, SQAURE_SIZE - 1);
     }
   }
-}
-
-function evole(prev: number[][]) {
-  let gol = prev.map((inner) => inner.slice());
-
-  const neighbors = (x: number, y: number) => {
-    let result = 0,
-      width = gol.length,
-      height = gol[0].length;
-    const cols = [x == 0 ? width - 1 : x - 1, x, x + 1 >= width ? 0 : x + 1];
-    const rows = [y == 0 ? height - 1 : y - 1, y, y + 1 >= height ? 0 : y + 1];
-
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (!(i == 1 && j == 1)) result += prev[cols[i]][rows[j]] ? 1 : 0;
-      }
-    }
-    return result;
-  };
-
-  for (let x = 0; x < gol.length; x++) {
-    for (let y = 0; y < gol[x].length; y++) {
-      const n = neighbors(x, y);
-      if (gol[x][y] == 0 && n == 3) gol[x][y] = 1;
-      if (gol[x][y] == 1 && !(n == 2 || n == 3)) gol[x][y] = 0;
-    }
-  }
-
-  return gol;
 }
 
 function init() {
-  const canvas = document.getElementById("playground") as HTMLCanvasElement;
   const w = document.body.clientWidth;
   const h = document.body.clientHeight;
 
   canvas.width = w;
   canvas.height = h;
 
-  var gol = setupGame(w, h) as number[][];
+  var gol = setupBoard(Math.ceil(w / SQAURE_SIZE), Math.ceil(h / SQAURE_SIZE), 0.85);
 
   function animate() {
     const fps = 10;
 
     drawBoard(gol);
-    gol = evole(gol);
+    gol = evolve(gol);
 
     setTimeout(() => {
       requestAnimationFrame(animate);
